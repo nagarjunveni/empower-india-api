9148df176a5146aedabf04d181daff05a5cb49d5
venkat <dandigam@gmail.com>
2025-04-18 10:42:13 -0400

    Finance changes

--- src/main/java/com/andhraempower/controller/FinanceController.java
+++ src/main/java/com/andhraempower/controller/FinanceController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 
 @CrossOrigin
 @RestController
